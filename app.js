48a612820a4a3f356429ee4c8330ec1609c51f2a
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-20 16:32:36 -0700

    grabbed html elements and stored them in variables in js, added event listener for button click

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // import functions and grab DOM elements
-
+import pokemon from './pokemon-list.js';
 // initialize global state
+const pokemon1 = document.getElementById('input1');
+const pokemonImg1 = document.getElementById('img1');
+const pokemon2 = document.getElementById('input2');
+const pokemonImg2 = document.getElementById('img2');
+const pokemon3 = document.getElementById('input3');
+const pokemonImg3 = document.getElementById('img3');
+const button = document.getElementById('select-pokemon');
 
 // set event listeners 
+button.addEventListener('click', () =>{
+  
+});
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
