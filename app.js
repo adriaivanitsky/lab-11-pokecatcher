6b4d485e56934f9c229ec9e3cafc42bee80be875
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-20 17:36:35 -0700

    got generate a random pokemon function workinggit add -Agit add -A

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // import functions and grab DOM elements
 import pokemon from './pokemon-list.js';
 // initialize global state
-const pokemon1 = document.getElementById('input1');
+let pokemon1 = document.getElementById('input1');
 const pokemonImg1 = document.getElementById('img1');
-const pokemon2 = document.getElementById('input2');
+let pokemon2 = document.getElementById('input2');
 const pokemonImg2 = document.getElementById('img2');
-const pokemon3 = document.getElementById('input3');
+let pokemon3 = document.getElementById('input3');
 const pokemonImg3 = document.getElementById('img3');
 const button = document.getElementById('select-pokemon');
 const generatePokemon = () => {
@@ -22,10 +22,18 @@ const generatePokemon = () => {
         randomNum3 = Math.floor(Math.random() * pokemon.length);
     }
     // console.log(pokemon[randomNum1], randomNum2, randomNum3)
+    pokemon1 = pokemon[randomNum1];
+    pokemonImg1.src = pokemon1['url_image'];
+    pokemon2 = pokemon[randomNum2];
+    pokemonImg2.src = pokemon2['url_image'];
+    pokemon3 = pokemon[randomNum3];
+    pokemonImg3.src = pokemon3['url_image'];
 };
-generatePokemon();
+
 // set event listeners 
 button.addEventListener('click', () =>{
+    generatePokemon();
+  
 });
 
   // get user input
