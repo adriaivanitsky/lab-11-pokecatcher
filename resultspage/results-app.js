29719d42cbb0c1b1d0c56148d25a5631ad5c189c
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-22 13:56:47 -0700

    added chart to results page

--- resultspage/results-app.js
+++ resultspage/results-app.js
@@ -31,8 +31,45 @@ for (let item of results){
 
 pickPokemon(1);
 
-// const names = results.map((item)=>{
-//     const pokemon = findById(item.id, pokemonList);
-//     return pokemon.name;
-// });
+const names = results.map((item)=>{
+    const pokemon = findById(item.id, pokemonList);
+    return pokemon.pokemon;
+});
+const picked = results.map(item=>item.picked);
 
+var ctx = document.getElementById('myChart').getContext('2d');
+// eslint-disable-next-line no-undef
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: names,
+        datasets: [{
+            label: '# of Times Picked',
+            data: picked,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
