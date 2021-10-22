1cec73c21b070f1bc5ecb46b59f530f1a239ef11
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-22 14:49:48 -0700

    got encountered graph to show up on results page

--- resultspage/results-app.js
+++ resultspage/results-app.js
@@ -37,14 +37,50 @@ const names = results.map((item)=>{
 });
 const picked = results.map(item=>item.picked);
 
-var ctx = document.getElementById('myChart').getContext('2d');
+var chosenBar = document.getElementById('chosen-bar').getContext('2d');
+var encounteredBar = document.getElementById('encountered-bar').getContext('2d');
 // eslint-disable-next-line no-undef
-new Chart(ctx, {
+new Chart(chosenBar, {
     type: 'bar',
     data: {
         labels: names,
         datasets: [{
-            label: '# of Times Picked',
+            label: '# of Times Chosen',
+            data: picked,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+// eslint-disable-next-line no-undef
+new Chart(encounteredBar, {
+    type: 'bar',
+    data: {
+        labels: names,
+        datasets: [{
+            label: '# of Times Encountered',
             data: picked,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
